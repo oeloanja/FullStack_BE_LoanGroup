60baaf867f95e9628f349ec641727bbf1074812e
platinum57 <sook8986@naver.com>
2024-11-15 16:49:51 +0900

    chore: entity와 config 수정

--- src/main/java/com/billit/loangroup_service/config/RedisConfig.java
+++ src/main/java/com/billit/loangroup_service/config/RedisConfig.java
@@ -1,24 +1,23 @@
 package com.billit.loangroup_service.config;
 
+import com.billit.loangroup_service.entity.PlatformAccount;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
-    }
-
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+    public RedisTemplate<String, PlatformAccount> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, PlatformAccount> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 
--- src/main/java/com/billit/loangroup_service/entity/PlatformAccount.java
+++ src/main/java/com/billit/loangroup_service/entity/PlatformAccount.java
@@ -1,20 +1,48 @@
 package com.billit.loangroup_service.entity;
 
-import jakarta.persistence.Id;
-import org.springframework.data.redis.core.RedisHash;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@RedisHash("PlatformAccount")
-public class PlatformAccount implements Serializable {
+@NoArgsConstructor
+@Entity
+@Table(name = "platform_account")
+public class PlatformAccount {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long platformAccountId;
 
-    private Long groupId;
-    private Long requiredAmount;
-    private Long currentBalance;
+    @Column(nullable = false)
+    private Integer groupId;
+
+    @Column(nullable = false, length = 50)
+    private BigDecimal requiredAmount;
+
+    @Column(precision = 15, scale = 2, nullable = false)
+    private BigDecimal currentBalance;
+
+    @Column(nullable = false)
     private Boolean isClosed = false;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    public PlatformAccount(Integer groupId, BigDecimal requiredAmount, BigDecimal currentBalance) {
+        this.groupId = groupId;
+        this.requiredAmount = requiredAmount;
+        this.currentBalance = currentBalance;
+        this.isClosed = false;
+    }
+
+    public void updateBalance(BigDecimal amount) {
+        this.currentBalance = this.currentBalance.add(amount);
+        if (this.currentBalance.compareTo(this.requiredAmount) >= 0) {
+            this.isClosed = true;
+        }
+    }
 }
+
