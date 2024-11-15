f47dee7eb82b6f7303c122baeee8b0319cef5513
platinum57 <sook8986@naver.com>
2024-11-15 17:00:06 +0900

    feat: cache 클래스 추가

---
+++ src/main/java/com/billit/loangroup_service/cache/PlatformAccountCache.java
@@ -0,0 +1,19 @@
+package com.billit.loangroup_service.cache;
+
+import com.billit.loangroup_service.entity.PlatformAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlatformAccountCache {
+    private RedisTemplate<String, PlatformAccount> redisTemplate;
+
+    public void saveToCache(String key, PlatformAccount account) {
+        redisTemplate.opsForValue().set(key, account);
+    }
+
+    public PlatformAccount getFromCache(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
