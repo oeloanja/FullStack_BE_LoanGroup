5d03390f740d166d71c2c13195344fff907149b3
platinum57 <sook8986@naver.com>
2024-11-15 14:32:37 +0900

    chore: first commit

---
+++ src/main/java/com/billit/loangroup_service/LoangroupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.billit.loangroup_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoangroupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoangroupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billit/loangroup_service/LoangroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billit.loangroup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoangroupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
